bcf8f1341aa1e9ba00731d74248d195ef4f02c7c
Hoan Tiny <thedevil093@gmail.com>
2024-05-13 17:40:00 +0700

    Update app.scss, site.js, and Course model, and fix db connection

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,30 +1,34 @@
-const Course = require('../models/Course');
-const { mutipleMongooseToObject } = require('../../util/mongoose');
+const Course = require("../models/Course");
+const { mutipleMongooseToObject } = require("../../util/mongoose");
 
 class MeController {
-    // [GET] /stored/courses
-    storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) => {
-                res.render('me/stored-courses', {
-                    courses: mutipleMongooseToObject(courses),
-                });
-            })
-            .catch(next);
-    }
+  // [GET] /stored/courses
+  storedCourses(req, res, next) {
+    Promise.all([
+      Course.find({}),
+      Course.countDocumentsWithDeleted({ deleted: true }),
+    ])
+      .then(([courses, deleteCount]) =>
+        res.render("me/stored-courses", {
+          deleteCount,
+          courses: mutipleMongooseToObject(courses),
+        })
+      )
+      .catch(next);
+  }
 
-    // [GET] /me/trash/courses
-    trashCourses(req, res, next) {
-        Course.findDeleted({ deleted: true })
-            .then((courses) => {
-                res.render('me/trash-courses', {
-                    courses: mutipleMongooseToObject(
-                        courses.filter((course) => course.deleted),
-                    ),
-                });
-            })
-            .catch(next);
-    }
+  // [GET] /me/trash/courses
+  trashCourses(req, res, next) {
+    Course.findDeleted({ deleted: true })
+      .then((courses) => {
+        res.render("me/trash-courses", {
+          courses: mutipleMongooseToObject(
+            courses.filter((course) => course.deleted)
+          ),
+        });
+      })
+      .catch(next);
+  }
 }
 //
 module.exports = new MeController();
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,16 +1,16 @@
 // Using Node.js `require()`
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connect() {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/f8_edu', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connect successfully!!!');
-    } catch (error) {
-        console.log('Connect failure!!!');
-    }
+  try {
+    await mongoose.connect("mongodb://localhost:27017/f8_education_dev", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connect successfully!!!");
+  } catch (error) {
+    console.log("Connect failure!!!");
+  }
 }
 
 module.exports = { connect };
