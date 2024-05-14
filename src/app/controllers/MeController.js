0e59564d2bde5f815f7ac84beab0dfa887535d80
Hoan Tiny <thedevil093@gmail.com>
2024-05-14 17:09:50 +0700

    Add middlewares and open-iconic icons for various UI elements

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,34 +1,42 @@
-const Course = require("../models/Course");
-const { mutipleMongooseToObject } = require("../../util/mongoose");
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class MeController {
-  // [GET] /stored/courses
-  storedCourses(req, res, next) {
-    Promise.all([
-      Course.find({}),
-      Course.countDocumentsWithDeleted({ deleted: true }),
-    ])
-      .then(([courses, deleteCount]) =>
-        res.render("me/stored-courses", {
-          deleteCount,
-          courses: mutipleMongooseToObject(courses),
-        })
-      )
-      .catch(next);
-  }
+    // [GET] /stored/courses
+    storedCourses(req, res, next) {
+        let courseQuery = Course.find({});
 
-  // [GET] /me/trash/courses
-  trashCourses(req, res, next) {
-    Course.findDeleted({ deleted: true })
-      .then((courses) => {
-        res.render("me/trash-courses", {
-          courses: mutipleMongooseToObject(
-            courses.filter((course) => course.deleted)
-          ),
-        });
-      })
-      .catch(next);
-  }
+        // if (req.query.hasOwnProperty("_sort")) {
+        //   courseQuery = courseQuery.sort({
+        //     [req.query.column]: req.query.type,
+        //   });
+        // }
+
+        Promise.all([
+            courseQuery,
+            Course.countDocumentsWithDeleted({ deleted: true }),
+        ])
+            .then(([courses, deleteCount]) =>
+                res.render('me/stored-courses', {
+                    deleteCount,
+                    courses: mutipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next);
+    }
+
+    // [GET] /me/trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({ deleted: true })
+            .then((courses) => {
+                res.render('me/trash-courses', {
+                    courses: mutipleMongooseToObject(
+                        courses.filter((course) => course.deleted),
+                    ),
+                });
+            })
+            .catch(next);
+    }
 }
 //
 module.exports = new MeController();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 var methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
+// const middlewares = require("middlewares");
 
 const app = express();
 const port = 3006;
@@ -11,6 +12,8 @@ const db = require('./config/db');
 
 // Connect to DB
 db.connect();
+
+console.log('db', db);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,6 +26,9 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+// Use Middlewares
+app.use(require('./app/middlewares/SortMiddlewares'));
+
 // app.engine("handlebars", handlebars());
 app.engine(
     'hbs',
@@ -31,6 +37,26 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+                return `<a href="?_sort&column=${field}&type=${type}"><span class="${icon}"></span></a>`;
+            },
         },
     }),
 );
