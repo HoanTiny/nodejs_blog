646e7419c3aba1a54acf4f903f65b884e5c4c36c
Hoan Tiny <thedevil093@gmail.com>
2024-05-15 17:36:03 +0700

    Update npm dependencies and add mongoose-sequence for auto-incrementing course IDs

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 var mongoose_delete = require('mongoose-delete');
 var slug = require('mongoose-slug-updater');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema(
     {
+        _id: { type: Number },
         name: { type: String, required: true },
         description: { type: String },
         image: { type: String },
@@ -14,9 +16,23 @@ const Course = new Schema(
         slug: { type: String, slug: 'name', unique: true },
     },
     {
+        _id: false,
         timestamps: true,
     },
 );
+
+// Custom query helpers
+Course.query.sortable = function (req) {
+    if (req.query.hasOwnProperty('_sort')) {
+        const isValidtype = ['asc', 'desc'].includes(req.query.type);
+        return this.sort({
+            [req.query.column]: isValidtype ? req.query.type : 'asc',
+        });
+    }
+    return this;
+};
+
+Course.plugin(AutoIncrement);
 
 mongoose.plugin(slug);
 Course.plugin(mongoose_delete, {
